3f2b423d7ddd15236ece10575be749f74de20a57
umuterkus <umuterkusofficial@gmail.com>
2025-12-25 10:39:11 +0300

    Player Collision Added

--- Flappy Bird Clone/Assets/_Scripts/Player/PlayerCollision.cs
+++ Flappy Bird Clone/Assets/_Scripts/Player/PlayerCollision.cs
@@ -1,16 +1,40 @@
+using System;
 using UnityEngine;
+
 
 public class PlayerCollision : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public event Action OnBirdDied;
+    public event Action OnScoreGained;
+
+    bool isDead = false;
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (isDead) return;
+
+        if (collision.gameObject.TryGetComponent(out ICollidable hitObject))
+        {
+            hitObject.OnPlayerHit();
+            Die();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (isDead) return;
+
+        if (other.TryGetComponent(out ICollidable hitObject)) 
+        {
+            
+            OnScoreGained?.Invoke();
+        }
     }
+
+    private void Die()
+    {
+        isDead = true;
+        
+        OnBirdDied?.Invoke();
+    }
+
 }
--- Flappy Bird Clone/Assets/_Scripts/Player/PlayerMovement.cs
+++ Flappy Bird Clone/Assets/_Scripts/Player/PlayerMovement.cs
@@ -10,7 +10,6 @@ public class PlayerMovement : MonoBehaviour
         
     }
 
-  
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
